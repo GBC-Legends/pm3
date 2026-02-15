e0fc94e8b0fb6816f9f3e4a955b255e02454ed33
Fidan Zeynalli <fidan.zeynalli@georgebrown.ca>
2026-02-15 18:52:48 -0500

    Add TCP ping command to communicate with the daemon

--- pm3/src/main.rs
+++ pm3/src/main.rs
@@ -1,3 +1,22 @@
+mod tcp_connector;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Ping,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Ping => tcp_connector::ping_server(),
+    }
 }
