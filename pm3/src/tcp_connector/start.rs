01b230549b440745d1b839d229baf1a6606341b2
StegaDP <stegailodp@gmail.com>
2026-02-18 17:17:02 -0500

    Implement URL encoder for NewProcessConfig
    Replace the `to_url_encoded` stub with a percent-encoding
    implementation that encodes all fields into a query string,
    and use it in `start_program` output.

--- pm3/src/tcp_connector/start.rs
+++ pm3/src/tcp_connector/start.rs
@@ -27,8 +27,47 @@ impl NewProcessConfig {
     }
 
     pub fn to_url_encoded(&self) -> String {
-        // 5 - написать безопасный энкодер
-        todo!("FIDAN RABOTAI")
+        fn enc(s: &str) -> String {
+            let mut out = String::with_capacity(s.len() + s.len() / 2);
+            for &b in s.as_bytes() {
+                let ok = matches!(b,
+                    b'A'..=b'Z' |
+                    b'a'..=b'z' |
+                    b'0'..=b'9' |
+                    b'-' | b'.' | b'_' | b'~'
+                );
+                if ok {
+                    out.push(b as char);
+                } else {
+                    out.push('%');
+                    out.push_str(&format!("{:02X}", b));
+                }
+            }
+            out
+        }
+
+        fn push_kv(dst: &mut String, key: &str, val: &str) {
+            if !dst.is_empty() {
+                dst.push('&');
+            }
+            dst.push_str(key);
+            dst.push('=');
+            dst.push_str(&enc(val));
+        }
+
+        let mut q = String::new();
+
+        push_kv(&mut q, "proc_name", &self.proc_name);
+        push_kv(&mut q, "exec_dir", &self.exec_dir.to_string_lossy());
+        push_kv(&mut q, "exec_name", &self.exec_name);
+        push_kv(&mut q, "active", if self.active { "1" } else { "0" });
+
+        if !self.exec_args.is_empty() {
+            let joined = self.exec_args.join(" ");
+            push_kv(&mut q, "args", &joined);
+        }
+
+        q
     }
 }
 
@@ -57,6 +96,6 @@ pub fn start_program(
 
     let new_process = NewProcessConfig::new(proc_name, exec_dir, exec_name, exec_args);
 
-    println!("{new_process:?}");
+    println!("{:?}", new_process.to_url_encoded());
     // 6 - вынести стрим в функцию и передать ему `format!("START {}", new_process.to_url_encoded())`
 }
