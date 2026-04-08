504b9e73c0792c99e230f647767009421b525e8d
Fidan Zeynalli <fidanzeynalli30@gmail.com>
2026-04-07 21:01:51 -0400

    Add version flag handling.
    Handle -v, -V, and --version manually to display package version.

--- pm3/src/main.rs
+++ pm3/src/main.rs
@@ -1,7 +1,14 @@
 use clap::Parser;
-use pm3::{process_commands, Cli};
+use pm3::{Cli, process_commands};
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() == 2 && (args[1] == "-v" || args[1] == "-V" || args[1] == "--version") {
+        println!("pm3 {}", env!("CARGO_PKG_VERSION"));
+        return;
+    }
+
     let cli = Cli::parse();
 
     process_commands(cli.command)
